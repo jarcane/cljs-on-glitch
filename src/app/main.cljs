8b84deb9a9ea52cfb16167f448f60b6dfb4d9189
Annaia Berry <jarcane@gmail.com>
2018-08-30 12:50:34 +0300

    Fix CLJS issues

--- src/app/main.cljs
+++ src/app/main.cljs
@@ -1,19 +1,17 @@
 (ns app.main
   (:require [hyperapp :refer [h app]]))
 
-(def state (clj->js {:count 0}))
+(def state #js{:count 0})
 
 (def actions
-  (clj->js {:down (fn [value]
-                   (fn [state]
-                     {:count (- state.count value)}))
-            :up (fn [value]
-                  (fn [state]
-                    {:count (+ state.count value)}))}))
+  #js{:down (fn [state]
+              {:count (- (.-count state) 1)})
+      :up (fn [state]
+            {:count (+ (.-count state) 1)})})
 
 (defn view [state actions]
-  (h "div" #js{} #js[(h "h1" #js{} state.count)
-                     (h "button" #js{:onclick ((:up actions) 1)} "+")
-                     (h "button" #js{:onclick ((:down actions) 1)} "-")]))
+  (h "div" #js{} #js[(h "h1" #js{} (.-count state))
+                     (h "button" #js{:onclick (.-up actions)} "+")
+                     (h "button" #js{:onclick (.-down actions)} "-")]))
 
 (app state actions view js/document.body)
