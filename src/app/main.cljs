cabfe06892515408116483ffac0ee26297ed518d
Annaia Berry <jarcane@gmail.com>
2018-08-30 18:59:10 +0300

    Further fixes to main.cljs

--- src/app/main.cljs
+++ src/app/main.cljs
@@ -4,14 +4,16 @@
 (def state #js{:count 0})
 
 (def actions
-  #js{:down (fn [state]
-              {:count (- (.-count state) 1)})
-      :up (fn [state]
-            {:count (+ (.-count state) 1)})})
+  #js{:down (fn [_]
+              (fn [state]
+                #js{:count (- (.-count state) 1)}))
+      :up (fn [_]
+            (fn [state]
+              #js{:count (+ (.-count state) 1)}))})
 
 (defn view [state actions]
   (h "div" #js{} #js[(h "h1" #js{} (.-count state))
                      (h "button" #js{:onclick (.-up actions)} "+")
                      (h "button" #js{:onclick (.-down actions)} "-")]))
 
-(app state actions view js/document.body)
+(app state actions view (js/document.getElementById "app"))
